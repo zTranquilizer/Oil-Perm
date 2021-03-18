1bf83dae54fcc590892236131442e64fd4c054e1
Tranquilizer <irebel220@gmail.com>
2021-03-18 16:15:54 +0300

    added slider, and fixed blog with our work

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,7 +1,15 @@
 
+@@include('swiper-bundle.js', ());
 
 document.addEventListener("DOMContentLoaded", function () {
 	menuOpen();
+
+	let reviewsSlider = new Swiper('.reviews__slider', {
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+	})
 });
 
 function menuOpen() {
